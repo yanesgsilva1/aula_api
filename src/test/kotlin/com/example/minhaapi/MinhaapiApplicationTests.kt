ff3f2c6d02b662f5882954d42925dc9d26588c14
yanesgsilva1 <anysgs@gmail.com>
2025-10-21 23:01:50 -0300

    commit inicial

---
+++ src/test/kotlin/com/example/minhaapi/MinhaapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.minhaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinhaapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
